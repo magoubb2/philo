fa289eb3628238c53c01c75afdd57320b4c08578
Marguerite Baron Béliveau <margueritebaronbeliveau@Marguerites-MacBook-Air.local>
2023-09-13 16:22:52 -0400

    fixed ft_atoi

--- parsing_utils.c
+++ parsing_utils.c
@@ -6,7 +6,7 @@
 /*   By: margueritebaronbeliveau <margueritebaro    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 17:42:44 by margueriteb       #+#    #+#             */
-/*   Updated: 2023/09/13 15:54:53 by margueriteb      ###   ########.fr       */
+/*   Updated: 2023/09/13 16:21:31 by margueriteb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,42 @@ int ft_isdigit(char *str)
     return (-1);
 }
 
+static int	ft_space(char c)
+{
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r'
+		|| c == ' ')
+		return (1);
+	else
+	{
+		return (0);
+	}
+}
+
 int	ft_atoi(const char *str)
 {
-	int	n;
-	int	res;
 	int	i;
+	int	sign;
+	int	result;
 
-	n = 1;
-	res = 0;
 	i = 0;
-	if (!str)
+	sign = 1;
+	result = 0;
+	if (str == NULL)
 		return (0);
-	while (str[i] && (str[i] != '\t' && str[i] != '\n' \
-     && str[i] != '\v' && str[i] != '\f' && str[i] != '\r' && str[i] != ' '))
+	while (ft_space(str[i]) == 1)
 		i++;
-	if (str[i] == '-' || str[i] == '+')
+	if (str[i] == '-')
 	{
-		if (str[i] == '-')
-			n = n * -1;
+		sign = -1;
 		i++;
 	}
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
+	else if (str[i] == '+')
+		i++;
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
 	{
-		res = res * 10 + str[i] - '0';
+		result *= 10;
+		result += str[i] - '0';
 		i++;
 	}
-	return (n * res);
+	return (result * sign);
 }
